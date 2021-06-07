aeef5c0332fbb71c03bc613383a631ba8dd069ab
Oxidda <gamesfreak16@hotmail.com>
2021-06-08 00:09:36 +0200

    yes

--- Leatherback.Test/Startup.cs
+++ Leatherback.Test/Startup.cs
@@ -32,7 +32,7 @@ namespace Turtle.Test
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
